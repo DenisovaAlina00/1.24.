e08959de47d3c37a65f869da3a6052d730f1bb24
DenisovaAlina00 <113889282+DenisovaAlina00@users.noreply.github.com>
2023-02-15 09:49:17 +0300

    Add files via upload

---
+++ ConsoleApp14/ConsoleApp14/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+public class MainClass
+{
+    public static void Main()
+    {
+        double a, x;
+        Console.WriteLine("Введите значение а:");
+        a= int.Parse(Console.ReadLine());
+        x = Math.Sqrt((2 * a + Math.Sin(3 * a)) / 3.56);
+        Console.WriteLine("Ответ:{0}", x);
+    }
+}
+
---
+++ ConsoleApp15/ConsoleApp15/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+public class MainClass
+{
+    public static void Main()
+    {
+        double y, x;
+        Console.WriteLine("Введите значение х:");
+        x=int.Parse(Console.ReadLine());
+        y = Math.Sin((3.2 + Math.Sqrt(1 + x)) / 5);
+        Console.WriteLine("Ответ:{0}",y);
+    }
+}
